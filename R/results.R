739691029e63086a1f6f51063dba867c43b2e5df
timostenz <timo.stenz@web.de>
2024-10-24 16:29:16 +0200

    Do not call SBC_fit_to_diagnostics when 'algorithm == "meanfield"'.
    Diagnostics by SBC_fit_to diagnostics are meant for "sampling" not "meanfield".

--- R/results.R
+++ R/results.R
@@ -735,8 +735,10 @@ compute_SBC_single <- function(vars_and_generated, backend, cores,
           generated = generated, dquants = dquants,
           backend = backend)
 
-        res$backend_diagnostics <- SBC::SBC_fit_to_diagnostics(
-          fit, res$output, res$messages, res$warnings)
+        if(fit$algorithm != "meanfield") {
+          res$backend_diagnostics <- SBC::SBC_fit_to_diagnostics(
+            fit, res$output, res$messages, res$warnings)
+        }
         NULL
       }, error = identity)
     })
