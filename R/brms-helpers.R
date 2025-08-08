ce8cc5df3ec932f785df55de96772f8fd6c5db54
Martin Modrák <modrak.mar@gmail.com>
2025-08-08 21:00:59 +0200

    Fixed bug in brms + rstan

--- R/brms-helpers.R
+++ R/brms-helpers.R
@@ -116,9 +116,9 @@ variational_backend_from_stanmodel <- function(stanmodel, args) {
 
 brmsfit_from_stanfit <- function(fit, brmsargs) {
   fit_brms <- do.call(brms::brm, combine_args(brmsargs, list(empty = TRUE)))
-  variables <- fit$metadata()$stan_variables
   exclude <- brms:::exclude_pars(fit_brms)
   if(inherits(fit, "CmdStanFit")) {
+    variables <- fit$metadata()$stan_variables
     fit_brms$fit <- brms::read_csv_as_stanfit(fit$output_files(),
       variables = variables, exclude = exclude, algorithm = fit_brms$algorithm)
     if(inherits(fit, "CmdStanVB")) {
